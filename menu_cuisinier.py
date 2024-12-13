1818ad5edc92d46dddd3b25e39cc46ff58eeba87
Ingrid Stemer <ingrid.stemer@live.fr>
2024-12-13 16:37:21 +0100

    update file

--- menu_cuisinier.py
+++ menu_cuisinier.py
@@ -7,7 +7,10 @@ def menu_cuisiner():
     # afficher les commandes en status waiting
     choice_command = input("Choisissez le numéro de la commande à traiter : ")
     # afficher la commande sélectionner via l'id entrer par le cuisiner
-    Product.get_one_product(argument)
+    Product.get_product_with_ingredients(name)
     update_status = input(
         "Indique à tous que tu cuisines ces plats avec le statut 'cooking' : ")
     # update le status de la commande
+    # récupérer la liste des ingrédients présents dans get_product_with_ingredient
+    # boucle pour la MAJ du stock des ingredients
+    Ingredient.update_ingredients(id)
