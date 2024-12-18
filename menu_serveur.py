dea2dfb128f4fddaa2fa0a9b37f1a803d9489448
Sophie Théréau <sophiethereau@hotmail.fr>
2024-12-18 19:31:23 +0100

    server can now check on products and ingredients stocks

--- menu_serveur.py
+++ menu_serveur.py
@@ -1,5 +1,6 @@
 from products import Product
 from commands import Command
+from ingredients import Ingredient
 from settings import connect_db
 conn = connect_db()
 cursor = conn.cursor() 
@@ -13,8 +14,8 @@ def menu_serveur():
         print("2. Lire les commandes")
         # print("3. Mettre à jour une commande")
         # print("4. Créer un produit")
-        # print("5. Lire les produits")
-        # print("6. Lire les ingrédients")
+        print("5. Lire les produits")
+        print("6. Consulter les ingrédients et les stocks")
         print("7. Quitter")
 
         choix = input("Choisissez une option : ")
@@ -28,12 +29,12 @@ def menu_serveur():
             cursor.execute("SELECT id FROM commands ORDER BY id DESC LIMIT 1")
             command_id= int(cursor.fetchone()[0])
             
-            print(command_id)
+            print(f"La commande n°{command_id} a bien été créée.")
             
             while True:
                 Product.get_products()
-                product_id = int(input('Entrer un numéro de plat : '))
-                quantity = int(input("Choisissez une quantité."))
+                product_id = int(input('Entrer un numéro de produit commandé par le client : '))
+                quantity = int(input("Choisissez une quantité : "))
                 Command.add_product_to_command(int(command_id), int(product_id), int(quantity))               
                 add_product = input("Voulez-vous ajouter un produit? Oui ou non?")
                 if (add_product == "non"):
@@ -44,6 +45,14 @@ def menu_serveur():
       
         elif choix == "2":
             Command.get_commands()
+
+        elif choix == "5":
+            Product.get_products()
+            product_id = input("Choisissez le numéro du produit que vous souhaitez consulter : ")
+            Product.get_product_with_ingredients(product_id)
+        
+        elif choix == "6":
+            Ingredient.get_ingredients()
             
         elif choix == "7":
             print("Retour au menu principal...")
